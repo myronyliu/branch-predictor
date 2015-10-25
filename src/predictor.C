c72294a83be0089ea664f605e2555cf96c5da8e5
yshmnks <myronyliu@gmail.com>
2015-10-25 03:22:28 -0700

    finished coding, but need to check to see if I made any mistakes. Seems to make more accurate predictions than baselines.

--- src/predictor.C
+++ src/predictor.C
@@ -1,14 +1,95 @@
-// A very stupid predictor.  It will always predict not taken.
+#include "predictor.h"
+
+inline int abs(int num)
+{
+    if (num < 0) return -num;
+    return num;
+}
+
+inline int clamp(int lower, int value, int upper)
+{
+    if (value < lower) return lower;
+    if (value > upper) return upper;
+    return value;
+}
 
 void init_predictor ()
 {
+    // Initialize all history bits and addresses to 0
+    for (int i = 0; i < h; ++i)
+    {
+	GHR[i] = 0;
+	GA[h] = 0;
+    }
+
+    // Initialize all weights to 0
+    for (int i = 0; i < n; ++i)
+    {
+	for (int j = 0; j < m; ++j)
+	{
+	    for (int k = 0; k < h; ++k)
+	    {
+		W[i][j][k] = 0;
+	    }
+	}
+	bias[i] = 0;
+    }
 }
 
 bool make_prediction (unsigned int pc)
 {
-  return false;
+    int B = pc % n; // modded address of the branch to be predicted
+
+    int y = bias[B]; // initialize the prediction to the bias
+
+    for (int i = 0; i < h; ++i) // accumulate the dot product of history with weights
+    {
+	y += W[B][GA[i]][i] * GHR[i];
+    }
+    return y >= 0;
 }
 
 void train_predictor (unsigned int pc, bool outcome)
 {
+    int B = pc % n; // modded address of the branch to be predicted
+
+    int sgn = 2*outcome - 1; // the outcome converted to +1 -1
+
+    ////////////////////////////
+    //// Get the prediction ////
+    ////////////////////////////
+
+    int y = bias[B];
+
+    for (int i = 0; i < h; ++i)
+    {
+	y += W[B][GA[i]][i] * GHR[i];
+    }
+    bool t = (y >= 0);
+
+    ///////////////////////////////////////////////////////////////////////////
+    //// Update weights if we predicted incorrectly or with low confidence ////
+    ///////////////////////////////////////////////////////////////////////////
+
+    if (t != outcome || abs(y) < theta)
+    {
+	for (int i = 0; i < h; ++i)
+	{
+	    W[B][GA[i]][i] = clamp(wMin, W[B][GA[i]][i] + sgn * GHR[i], -wMin - 1);
+	    // i.e. increment W[B][GA[i]][i] if GHR[i] == t; decrement otherwise
+	}
+	bias[B] = clamp(wMin, bias[B] + sgn, -wMin - 1);
+    }
+
+    ///////////////////////////
+    //// Update GHR and GA ////
+    ///////////////////////////
+
+    for (int i = 1; i < h; ++i)
+    {
+	GHR[i - 1] = GHR[i]; 
+	GA[i - 1] = GA[i];
+    }
+    GHR[h-1] = sgn;
+    GA[h-1] = B;
 }
--- src/predictor.h
+++ src/predictor.h
@@ -14,7 +14,48 @@
   concerned about the memory used by the simulated branch predictor.
 */
 
+////////////////////////////////////////////////////////
+//// The following predictor follows the paper      ////
+//// "Piecewise Linear Branch Prediction" (Jimenez) ////
+////////////////////////////////////////////////////////
 
+#define h 26	        // number of global history bits (dimension 3 of W)
+#define n 8		// number of unique branch PCs in dimension 1 of W
+#define m 118		// number of unique branch PCs in dimension 2 of W
+//#define theta 256	// threshhold (we train the predictor if theta is not reached)
+#define wBits 8		// weights can take on values in range [-2^(wBits-1), 2^(wBits-1)-1]
+#define wMin -128	// ... this must be consistent with wBits
+
+static float theta = 2.14*(h + 1) + 20.58;
+
+/*
+    W holds the weights.
+
+    Suppose we want to predict branch B given a history {GHR, GA}.
+    We look upon 2D array W[B].
+    Within W[B], history {GHR, GA} specifies h elements, each of which is a weight.
+    These weights are then fed into the perceptron formula.
+
+    For the overall bias of branch B, we look upon bias[B].
+
+    We use an array for GHR as well, so that we can extend the history beyond 64 bits.
+    Typically, long long int is sufficient though.
+*/
+
+static int W[n][m][h];
+static int bias[n]; // the bias of branch pc % n; 
+
+static int GHR[h];  // the global history of taken/not-taken (+1 and -1 respectively)
+static int GA[h];   // the branch addresses corresponding to each entry of GHR
+
+/*
+    The memory usage in bits is:
+	n*m*h*wBits	    for W
+	n*wBits		    for bias
+	h		    for GHR
+	h*log_2(n)	    for GA
+    where the extra bit in wBits+1 is to keep track of the sign
+*/
 
 
 /*
